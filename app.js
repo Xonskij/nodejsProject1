ec077ade07abbb2138bb6f003b085407cf95714a
Anatole <fgqfgqfgq654123@gmail.com>
2016-05-01 14:19:19 +0300

    First commit

--- app.js
+++ app.js
@@ -4,72 +4,44 @@ var scraper = require('./server/helpers/scraper-articles');
 var url = require('url');
 var article = require('./server/models/article-model');
 
-var server = http.createServer(function (request, response) {
-     var pathname = url.parse(request.url).pathname;
+    var server = http.createServer(function (request, response) {
+       var pathname = url.parse(request.url).pathname;
 
-        if (pathname == '/') {
+            if (pathname == '/') {
 
-            article.find({}, function(err, article) {
+            article.find({}, function(err, articles) {
                 if (err) {
-                    throw err;
-                }
+                                   throw err;
+                               }
 
+                                var page = articles.reduce(function (response, item) {
+                                    return response + '<h3>' + '<a href='+ item.id +'>' + item.title + '</a>' + '</h3>' + '<p>' + item.summary + '</p>';
+                               }, '');
 
-                var page = article.reduce(function (response, item) {
-
-                    var star = item.title;
-
-                    stars = star.split(" ");
-
-                    for(i = 0; i < stars.length; i++){
-                        if(stars[i].indexOf("Гродн") == -1){
-
-                        }
-                        else{
-
-                            var str1 = "";
-
-                            for(j = 0; j < stars[i].length; j++){
-                                str1 += "*";
-                            }
-                            stars[i] = str1;
-
-                        }
-                    }
-
-                    var st="";
-
-                    for(i = 0; i < stars.length; i++){
-                        st += stars[i] + " ";
-                    }
-
-                    return response + '<h3>' + '<a href='+ item.id +'>' + st + '</a>' + '</h3>' + '<p>' + item.summary + '</p>';
-                }, '');
-
-            response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-            response.end(page);
-        });
-    }
-    else {
-
-        article.findOne({id: pathname.replace('/', '')}, function (error, item) {
-
-            if(item != null) {
-                response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-                response.end(item.description);
+                               response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+                            response.end(page);
+                       });
             }
-            else {
-                response.writeHead(404, {'Content-Type': 'text/html; charset=utf8'});
-                response.end('Not found');
+        else {
+
+                    article.findOne({id: pathname.replace('/', '')}, function (error, item) {
+
+                                if(item != null) {
+                                    response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+                                    response.end(item.description);
+                               }
+                            else {
+                                   response.writeHead(404, {'Content-Type': 'text/html; charset=utf8'});
+                                    response.end('Not found');
+                                }
+                        })
+
             }
-        })
+    });
 
-    }
-});
+    setInterval(function() {
+            scraper.update();
+            console.log('Search...');
+        }, 10000);
 
-setInterval(function() {
-    scraper.update();
-    console.log('Search...');
-}, 10000);
-
-server.listen({port: serverConfig.port});
+    +server.listen({port: serverConfig.port});
